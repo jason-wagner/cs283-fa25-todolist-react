368d11b4bfa353a0ef0b932cc2fb1829dead02c0
Jason Wagner <jason.w.wagner@gmail.com>
2025-12-02 17:04:52 -0500

    Implement editing, number of items remaining, placeholders for lab

--- src/App.jsx
+++ src/App.jsx
@@ -53,6 +53,46 @@ function App() {
     setTodos(updatedTodos);
   }
 
+  function markAsEditing(id) {
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        todo.isEditing = true;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function updateTodo(event, id) {
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        if(event.target.value.trim().length === 0)
+          return todo;
+
+        todo.title = event.target.value.trim();
+        todo.isEditing = false;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  function cancelEditing(id) {
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        todo.isEditing = false;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
   return (
     <>
       <div className="min-h-[100vh] p-2.5 bg-slate-100">
@@ -72,16 +112,41 @@ function App() {
               <li key={todo.id} class="flex mt-6 items-center justify-between">
                 <div className="flex flex-1 items-center mr-6">
                   <input type="checkbox" onChange={() => completeTodo(todo.id)} checked={todo.isComplete} />
-                  <span class="ml-4">{todo.title}</span>
+                  { !todo.isEditing ? (
+                    <span
+                      onDoubleClick={() => markAsEditing(todo.id)}
+                      className={`ml-4 ${todo.isComplete ? 'line-through' : ''}`}>
+                        {todo.title}
+                    </span>
+                  ) : (
+                    <input
+                      type="text"
+                      className="ml-4 w-full border-0 shadow-md px-2 text-lg"
+                      defaultValue={todo.title}
+                      autoFocus
+                      onBlur={(event) => updateTodo(event, todo.id)}
+                      onKeyDown={event => {
+                        if (event.key === 'Enter') {
+                          updateTodo(event, todo.id)
+                        } else if (event.key === 'Escape') {
+                          cancelEditing(todo.id);
+                        }
+                      }}
+                    />
+                  )}
                 </div>
                 <button className="bg-white text-red-700" onClick={() => deleteTodo(todo.id)}>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                   </svg>
                 </button>
               </li>         
             ))}
           </ul>
+          <div className="flex justify-between items-center text-slate-600 mt-5 pt-4 border-t border-t-slate-200">
+              <button>Mark all as complete</button>
+              <span>{ todos.filter(todo => !todo.isComplete).length } items remaining</span>
+          </div>
         </div>
       </div>
     </>
