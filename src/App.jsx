ba1d07fe52e1f8cb4dfd6009f41fdc41bc0c2d96
Jason Wagner <jason.w.wagner@gmail.com>
2025-12-02 16:32:29 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: 'Do Lab 6 for CS 283', isComplete: false, isEditing: false },
+    {id: 2, title: 'Do laundry', isComplete: false, isEditing: false },
+    {id: 3, title: 'Go grocery shopping', isComplete: false, isEditing: false },
+  ]);
+
+  const [todoInput, setTodoInput] = useState('');
+  const [idForTodo, setIdForTodo] = useState(todos.length + 1);
+
+  function handleTodoInput(event) {
+    setTodoInput(event.target.value);
+  }
+
+  function addTodo(event) {
+    event.preventDefault();
+
+    if(todoInput.trim().length === 0)
+      return;
+
+    setTodos([
+      ...todos,
+      {
+        id: idForTodo,
+        title: todoInput.trim(),
+        isComplete: false,
+        isEditing: false,
+      }
+    ]);
+
+    setTodoInput('');
+    setIdForTodo(prevIdForTodo => prevIdForTodo + 1)
+  }
+
+  function deleteTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  function completeTodo(id) {
+    const updatedTodos = todos.map(todo => {
+      if(todo.id === id) {
+        todo.isComplete = true;
+      }
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <>
+      <div className="min-h-[100vh] p-2.5 bg-slate-100">
+        <div className="m-auto mt-[30px] p-8 bg-white rounded-md shadow-lg max-w-[512px]">
+          <h2 className="text-2xl font-bold">To-Do App</h2>
+
+          <form action="#" onSubmit={addTodo}>
+            <input type="text"
+                  className="full border-0 shadow p-3.5 text-lg mt-4"
+                  placeholder="New Item"
+                  value={todoInput}
+                  onChange={handleTodoInput}
+            />
+          </form>
+          <ul className="mt-8">
+            {todos.map((todo) => (
+              <li key={todo.id} class="flex mt-6 items-center justify-between">
+                <div className="flex flex-1 items-center mr-6">
+                  <input type="checkbox" onChange={() => completeTodo(todo.id)} checked={todo.isComplete} />
+                  <span class="ml-4">{todo.title}</span>
+                </div>
+                <button className="bg-white text-red-700" onClick={() => deleteTodo(todo.id)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  </svg>
+                </button>
+              </li>         
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
